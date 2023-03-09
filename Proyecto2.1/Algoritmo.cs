88e7a789c158bb078453dc7c4cc51db0714827d9
Manuel <bardalesfrancom@gmail.com>
2023-03-09 15:11:25 -0600

    Lecturas y arreglos funcionando, falta implementacion de algoritmos

--- Proyecto2.1/Algoritmo.cs
+++ Proyecto2.1/Algoritmo.cs
@@ -6,12 +6,22 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.MobileControls;
+using System.Windows.Documents;
 using System.Windows.Forms;
 
 namespace Proyecto2._1
 {
-    public partial class Algoritmo : Form
+    public partial class Algoritmo : System.Windows.Forms.Form
     {
+        //arreglos en los que se guardaran los datos de los marcos
+        private List<string> referencias;
+        private List<string> marco1;
+        private List<string> marco2;
+        private List<string> marco3;
+        private List<string> marco4;
+        //arreglo que guarda los fallos y no fallos
+        private List<string> resultados;
         private String direccion;
         public Algoritmo()
         {
@@ -35,7 +45,11 @@ namespace Proyecto2._1
 
         private void Algoritmo_Load(object sender, EventArgs e)
         {
-
+            referencias = ExcelLecturas.TraerDatos(2, 2, direccion);
+            marco1 = ExcelLecturas.TraerDatos(3, 2, direccion);
+            marco2 = ExcelLecturas.TraerDatos(4, 2, direccion);
+            marco3 = ExcelLecturas.TraerDatos(5, 2, direccion);
+            marco4 = ExcelLecturas.TraerDatos(6, 2, direccion);
         }
     }
 }
---
+++ Proyecto2.1/ExcelLecturas.cs
@@ -0,0 +1,32 @@
+﻿using SpreadsheetLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2._1
+{
+    class ExcelLecturas
+    {
+        /// <summary>
+        /// funcion que trae cualquier rango de datos en el documento
+        /// </summary>
+        
+        internal static List<string> TraerDatos(int fila, int columna, string direccion)
+        {
+            //traer el documento
+            SLDocument documento = new SLDocument(direccion);
+            List<string> datos = new List<string>();
+
+            //nuestra plantilla se encuentra en las primeras 16 columnas
+            for (int i =columna ; i <= 16; i++)
+            {
+                //agregamos al arreglo todos los valores de esa fila y la columna que se esta corriendo
+                datos.Add(documento.GetCellValueAsString(fila, i));
+            }
+
+            return datos;
+        }
+    }
+}
