e9d0a7f99f5ae941c121f04720620f9e3262725c
Manuel <bardalesfrancom@gmail.com>
2023-03-11 20:46:21 -0600

    Clase creada

---
+++ Proyecto2.1/Segundaoportunidad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2._1
+{
+    class Segundaoportunidad
+    {
+        private List<string> referencias;
+        private List<string> marco1;
+        private List<string> marco2;
+        private List<string> marco3;
+        private List<string> marco4;
+        private List<string> bitsM1;
+        private List<string> bitsM2;
+        private List<string> bitsM3;
+        private List<string> bitsM4;
+
+        public Segundaoportunidad(List<string> referencias, List<string> marco1, List<string> marco2, List<string> marco3, List<string> marco4, List<string> bitsM1, List<string> bitsM2, List<string> bitsM3, List<string> bitsM4)
+        {
+            this.referencias = referencias;
+            this.marco1 = marco1;
+            this.marco2 = marco2;
+            this.marco3 = marco3;
+            this.marco4 = marco4;
+            this.bitsM1 = bitsM1;
+            this.bitsM2 = bitsM2;
+            this.bitsM3 = bitsM3;
+            this.bitsM4 = bitsM4;
+        }
+    }
+}
