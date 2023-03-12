f31e11685529bb1944984f9df09c147e2cd4387e
ADAN ALEXANDER RIVAS LAINEZ <adanrivas@unitec.edu>
2023-03-12 15:57:53 -0600

    Creacion de Clase Reloj

--- Proyecto2.1/Reloj.cs
+++ Proyecto2.1/Reloj.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Proyecto2._1
 {
-    internal class Reloj
+    class reloj
     {
+        private List<string> referencias;
+        private List<string> marco1;
+        private List<string> marco2;
+        private List<string> marco3;
+        private List<string> marco4;
+        private List<string> bitsM1;
+        private List<string> bitsM2;
+        private List<string> bitsM3;
+        private List<string> bitsM4; 
+
+        public reloj(List<string> referencias, List<string> marco1, List<string> marco2, List<string> marco3, List<string> marco4, List<string> bitsM1, List<string> bitsM2, List<string> bitsM3, List<string> bitsM4)
+        {
+            this.referencias = referencias;
+            this.marco1 = marco1;
+            this.marco2 = marco2;
+            this.marco3 = marco3;
+            this.marco4 = marco4;
+
+            this.bitsM1 = bitsM1;
+            this.bitsM2 = bitsM2;
+            this.bitsM3 = bitsM3;
+            this.bitsM4 = bitsM4;
+        } 
+
+        internal bool comprobarAlgoritmo()
+        {
+            bool concuerda = false;
+           
+            if ()
+            {
+
+            }
+
+
+            return concuerda;
+        }
     }
 }
